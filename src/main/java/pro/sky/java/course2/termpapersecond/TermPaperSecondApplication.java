6b91a71fd86de63351deb7044202b0421d22b841
Gabriel_n3 <p705865@gmail.com>
2022-08-10 19:41:19 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/termpapersecond/TermPaperSecondApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.termpapersecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermPaperSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TermPaperSecondApplication.class, args);
+    }
+
+}
